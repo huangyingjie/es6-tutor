24e33e8ecfc92134ba0d876572c06220f7b28e5d
huangyingjie <huangyingjie@meituan.com>
2016-01-10 01:17:17 +0800

    添加运行命令，完善文档

--- test/ReflectAPI.js
+++ test/ReflectAPI.js
@@ -1,7 +1,7 @@
 import {assert} from "chai";
 describe("Reflect API", function () {
   it("Reflect API", function () {
-    var O = {a: 1};
+    const O = {a: 1};
     Object.defineProperty(O, 'b', {value: 2});
     O[Symbol('c')] = 3;
     assert.equal(O.a, 1);
