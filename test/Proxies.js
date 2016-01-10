818693adb2758e5fdcaebad3bb9dd80a25740743
huangyingjie <huangyingjie@meituan.com>
2016-01-10 23:51:34 +0800

    添加基于proxy实现的逆天功能 http://www.infoq.com/cn/articles/es6-in-depth-proxies-and-reflect

--- test/Proxies.js
+++ test/Proxies.js
@@ -13,4 +13,22 @@ describe("proxies", function () {
     obj.x = 1;
     assert.equal(obj.x, 1);
   });
+  it("add arbitrary props to object through proxy", function () {
+    function Tree() {
+      return new Proxy({}, handler);
+    }
+    const handler = {
+      get: function (target, key, receiver) {
+        if (!(key in target)) {
+          target[key] = Tree();  // 自动创建一个子树
+        }
+        return Reflect.get(target, key, receiver);
+      }
+    };
+    const tree = Tree();
+    tree.branch1.branch2.twig = "green";
+    assert.equal(tree.branch1.branch2.twig, "green");
+    tree.branch1.branch3.twig = "blue";
+    assert.equal(tree.branch1.branch3.twig, "blue");
+  });
 });
